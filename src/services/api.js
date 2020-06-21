b6c57ce28308af66e70bdb7a502b7617a1aa958c
Bruno Almeida <brunokfsop@gmail.com>
2020-06-20 22:35:55 -0300

    Removed API from api.js and used environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { API_SERVER } from 'react-native-dotenv';
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.5:3333/' //API_SERVER
+  baseURL: API_SERVER
 });
 
 export default api;
